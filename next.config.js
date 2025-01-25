a11ca722303f77e04d19dd77472d74f43c5794eb
Henry Apata <adebanji.apata@gmail.com>
2025-01-25 06:08:28 +0100

    Configure GitHub Pages deployment

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/Nexus_0x',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
